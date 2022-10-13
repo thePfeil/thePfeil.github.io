db4d51525dd3ec22f31354261f9df7ba83f96768
thePfeil <85781736+thePfeil@users.noreply.github.com>
2022-10-13 20:48:23 +0200

    initial commit

---
+++ phpTest.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>phpTest</title
+</head>
+<body>
+<button>
+  <a href="index.html">back to normal</a>
+</button>
+<p>from html</p>
+</body>
+</html>
+<?php
+  echo "from php";
+?>
