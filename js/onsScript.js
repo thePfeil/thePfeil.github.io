1775c10597d7f93c4b92920d70d1c8ff71c4bea8
thePfeil <85781736+thePfeil@users.noreply.github.com>
2024-02-09 09:11:02 +0100

    Update onsScript.js

--- js/onsScript.js
+++ js/onsScript.js
@@ -1,41 +1,41 @@
 function info()
    {
-     ons.notification.alert("Info", {buttonLabels:"nein", title:"!!!"});
+     ons.notification.alert("Info", {buttonLabels:"no", title:"!!!"});
    }
 
    const bestaetigung = function() {
     ons.platform.select("ios");
-    ons.notification.confirm("WARNUNG!!! Ihr IOS hat einen Virus! Jetzt löschen?", {buttonLabels:["nein", "ja"]})
+    ons.notification.confirm("WARNING!!! YOUR PHONE HAS A VIRUS! delete now?", {buttonLabels:["no", "yes"]})
        .then(function(sch) {
-         if(sch==0) ons.notification.alert("Abgebrohen");
-         else ons.notification.alert("Reinigung wird durchgeführt...").then( function() {
+         if(sch==0) ons.notification.alert("kanceled");
+         else ons.notification.alert("cleaning...").then( function() {
            ons.platform.select("android");
-           ons.notification.toast("DAS IST NICHT ECHT!!! Ernsthaft... geht gar nicht!", {timeout:2000})
+           ons.notification.toast("THAT'S NOT REAL!!! Man... stop.", {timeout:2000})
          } );
        });
    };
 
    const eingabe = function() {
-     ons.notification.prompt("Bitte eine Eingabe")
+     ons.notification.prompt("please enter text")
        .then(function(tx) {
-         if(tx=="") ons.notification.alert("Keine Eingabe");
-         else ons.notification.alert("Eingegeben: " + tx);
+         if(tx=="") ons.notification.alert("no input");
+         else ons.notification.alert("entered: " + tx);
        });
    };
 
    function toast()
    {
-     ons.notification.toast("Der Toast", {buttonLabel: "okay"});
+     ons.notification.toast("toast", {buttonLabel: "okay"});
    }
    
    const iosmode = function() {
-     ons.notification.confirm("welche OS?", {buttonLabels:["apple allg.", "windows+linux"]}).then(function(sss) {
+     ons.notification.confirm("which OS?", {buttonLabels:["ios", "android"]}).then(function(sss) {
        if(sss == 0) { 
          ons.platform.select("ios");
-         ons.notification.toast("ios modus", {buttonLabel: "okay"});
+         ons.notification.toast("ios mode", {buttonLabel: "okay"});
        } else {
          ons.platform.select("android");
-         ons.notification.toast("android modus", {buttonLabel: "okay"});
+         ons.notification.toast("android mode", {buttonLabel: "okay"});
        }
      });
    };
